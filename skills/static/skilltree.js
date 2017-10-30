2411c3e5ee3bbec0ca90a76f2980640cb00e6f02
deniauma <mathieu.deniaud@gmail.com>
2017-10-31 00:30:35 +0100

    Adding skill interaction

--- skills/static/skilltree.js
+++ skills/static/skilltree.js
@@ -1,3 +1,26 @@
+var skills = null;
+
+function get_selected_skill(){
+    var id = $('.selected').attr('id');
+    var skill = {};
+    skill.id = id;
+    skill.lvl = $('.selected > .skill_lvl').html();
+    for(s in skills){
+        if(skills[s].pk == id){
+            skill.fields = skills[s].fields;
+            break;
+        }
+    }
+    return skill;
+}
+
+function display_skill(){
+    var s = get_selected_skill();
+    $('#skill_title').html(s.fields.name);
+    $('#skill_effect1').html(s.fields['bonus_effect1_lvl'+s.lvl]);
+    $('#skill_effect2').html(s.fields.bonus_effect2_lvl1);
+}
+
 $(document).ready(function(){
 	
 	$('ul.tabs li').click(function(){
@@ -12,12 +35,16 @@ $(document).ready(function(){
     
     $('.skill_btn').bind('click', function(){
         console.debug($(this).parent().attr('id'));
+        $('.selected').removeClass('selected');
+        $(this).parent().addClass('selected');
+        display_skill();
 	})
 
     $('#skill_btn_upgrade').bind('click', function(){
         var lvl = $('.selected > .skill_lvl').html();
         lvl++;
         $('.selected > .skill_lvl').html(lvl++);
+        display_skill();
         console.debug(lvl);
     })
 
@@ -30,6 +57,9 @@ $(document).ready(function(){
         data = msg["skills"];
         s = JSON.parse(data);
         console.debug("Test: "+s.length+", "+s[0].pk)
+        skills = s;
     });
+
+    display_skill();
 
 })
