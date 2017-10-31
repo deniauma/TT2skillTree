c110f58d46d96cdab6b71b2a928b6eb72de7d82a
deniauma <mathieu.deniaud@gmail.com>
2017-10-31 11:50:20 +0100

    Adding restriction check on skills req

--- skills/static/skilltree.js
+++ skills/static/skilltree.js
@@ -1,10 +1,21 @@
 var skills = null;
+var knight = 0;
+var warlord = 0;
+var sorcerer = 0;
+var rogue = 0;
+var sp_used = 0;
+var build = {};
 
-function get_selected_skill(){
-    var id = $('.selected').attr('id');
+function init_build(){
+    for(s in skills){
+        build[skills[s].pk] = 0;
+    }
+}
+
+function get_skill(id){
     var skill = {};
     skill.id = id;
-    skill.lvl = $('.selected > .skill_lvl').html();
+    skill.lvl = build[id];
     for(s in skills){
         if(skills[s].pk == id){
             skill.fields = skills[s].fields;
@@ -14,11 +25,55 @@ function get_selected_skill(){
     return skill;
 }
 
+function get_selected_skill(){
+    var id = $('.selected').attr('id');
+    return get_skill(id);
+}
+
 function display_skill(){
     var s = get_selected_skill();
     $('#skill_title').html(s.fields.name);
     $('#skill_effect1').html(s.fields['bonus_effect1_lvl'+s.lvl]);
     $('#skill_effect2').html(s.fields.bonus_effect2_lvl1);
+}
+
+function upgrade_skill(){
+    var s = get_selected_skill();
+    var s_req = s.fields.skill_req;
+    var s_req_bool = true;
+    if(s_req != null){
+        req = get_skill(s_req);
+        if(req.lvl == 0){
+            s_req_bool = false;
+        }
+    }
+    var sp_req = s.fields.sp_req;
+    var tree_lvl = $('.tab-link.current > span').html()*1;
+    if(s.lvl*1 < s.fields.max_level && s_req_bool && tree_lvl >= sp_req){
+        var cost = s.fields['cost_lvl'+s.lvl];
+        sp_used += cost;
+    
+        if(s.fields.branch == "Red"){
+            knight += cost;
+            $('#knight_lvl').html(knight);
+        }
+        if(s.fields.branch == "Yellow"){
+            warlord += cost;
+            $('#warlord_lvl').html(warlord);
+        }
+        if(s.fields.branch == "Blue"){
+            sorcerer += cost;
+            $('#sorcerer_lvl').html(sorcerer);
+        }
+        if(s.fields.branch == "Green"){
+            rogue += cost;
+            $('#rogue_lvl').html(rogue);
+        }
+        build[s.id]++
+        $('.selected > .skill_lvl').html(build[s.id]);
+        $('#sp_used > span').html(sp_used);
+    }
+    
 }
 
 $(document).ready(function(){
@@ -30,7 +85,10 @@ $(document).ready(function(){
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
+        $("#"+tab_id).addClass('current');
+        $('.selected').removeClass('selected');
+        $("#"+tab_id+" > .skill_slot0").addClass('selected');
+        display_skill();
     })
     
     $('.skill_btn').bind('click', function(){
@@ -41,11 +99,8 @@ $(document).ready(function(){
 	})
 
     $('#skill_btn_upgrade').bind('click', function(){
-        var lvl = $('.selected > .skill_lvl').html();
-        lvl++;
-        $('.selected > .skill_lvl').html(lvl++);
+        upgrade_skill();
         display_skill();
-        console.debug(lvl);
     })
 
     $.ajax({
@@ -58,8 +113,7 @@ $(document).ready(function(){
         s = JSON.parse(data);
         console.debug("Test: "+s.length+", "+s[0].pk)
         skills = s;
+        init_build();
     });
-
-    display_skill();
 
 })
